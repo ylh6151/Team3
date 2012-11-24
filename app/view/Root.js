d6a7413cd311806b0cd5e2dd7ce654517d87a731
minju <minju@gmail.com>
2012-11-24 23:17:37 +0900

    border

--- app/view/Root.js
+++ app/view/Root.js
@@ -157,6 +157,7 @@ Ext.define('MyApp.view.Root', {
                             },
 							{
 								xtype:'button',
+								iconMask: true,
 								cls:'cls-star',
 								pressedCls:'cls-star2',
 								pressedDelay:2000,
